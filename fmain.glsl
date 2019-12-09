a1f3cecc51e7c34c274a5afcedf3871b7dc0e317
Tim Mason <Timothy.Mason@Colorado.edu>
2019-12-08 20:31:27 -0700

    Cleaner code for main shader

--- fmain.glsl
+++ fmain.glsl
@@ -1,45 +1,49 @@
 /****************************************************************************
 **
-** Fragment shader with per-pixel lighting and texture mapping
+** Fragment shader with per-pixel lighting and texture mapping with alpha-
+** channel based cutouts
 **
-** Adapted from "cube" example code from the Qt5 library, class example 27, 
-**  and some vermillion book thrown in.
-**
-** cube example: https://doc.qt.io/qt-5/qtopengl-cube-example.html
+** Adapted from example code:
+**   https://www.opengl.org/sdk/docs/tutorials/ClockworkCoders/lighting.php
 **
 ** Technique for texture cutouts based on alpha-channel:
 **   https://en.wikibooks.org/wiki/GLSL_Programming/Unity/Transparent_Textures
 **
 ****************************************************************************/
-varying vec2 v_texcoord;
-varying vec3 View;
-varying vec3 Light;
-varying vec3 Normal;
 
+uniform vec3 lightPosition;
 uniform sampler2D texture;
-uniform vec3 MatAmbient;
-uniform vec3 MatDiffuse;
-uniform vec3 MatSpecular;
+uniform vec4 MatAmbient;
+uniform vec4 MatDiffuse;
+uniform vec4 MatSpecular;
 uniform float MatShininess;
 
+varying vec2 v_texcoord;
+varying vec3 N;
+varying vec3 v;    
 
-void main()
-{
-    vec3 N = normalize(Normal);
-    vec3 L = normalize(Light);
-    vec3 R = reflect(-L,N);
-    vec3 V = normalize(View);
+void main (void)  
+{  
+    vec3 L = normalize(lightPosition - v);   
+    vec3 E = normalize(-v); // we are in Eye Coordinates, so EyePos is (0,0,0)  
+    vec3 R = normalize(-reflect(L,N));  
 
-    float Id = max(dot(L,N) , 0.0);
-    float Is = (Id>0.0) ? pow(max(dot(R,V),0.0) , MatShininess) : 0.0;
+    //calculate Ambient Term:  
+    vec4 Iamb = MatAmbient;    
 
-    // Sum color types
-    vec4 color = vec4(MatAmbient + Id*MatDiffuse + Is*MatSpecular, 1.0);
+    //calculate Diffuse Term:  
+    vec4 Idiff = MatDiffuse * max(dot(N,L), 0.0);
+    Idiff = clamp(Idiff, 0.0, 1.0);     
 
-    //  Apply texture
-    gl_FragColor = color * texture2D(texture,v_texcoord);
+    // calculate Specular Term:
+    vec4 Ispec = MatSpecular 
+                * pow(max(dot(R,E),0.0),0.3*MatShininess);
+    Ispec = clamp(Ispec, 0.0, 1.0); 
 
-    // Discard ("cutout") any fragments with post-texture alpha less than threshold 0.5
+    // write Total Color:  
+    gl_FragColor = (Iamb + Idiff + Ispec) * texture2D(texture,v_texcoord);
+
     if (gl_FragColor.a < 0.5)
         discard;
 }
+          
--- vmain.glsl
+++ vmain.glsl
@@ -2,32 +2,29 @@
 **
 ** Vertex shader with per-pixel lighting, texture mapping, and transparency
 **
-** Adapted from "cube" example code from the Qt5 library and class example 27
-**   https://doc.qt.io/qt-5/qtopengl-cube-example.html
+** Adapted from example code:
+**   https://www.opengl.org/sdk/docs/tutorials/ClockworkCoders/lighting.php
 **
 ****************************************************************************/
 
 uniform mat4 mvp_matrix;
 uniform mat4 mv_matrix;
 uniform mat3 normalMatrix;
-uniform vec3 lightPosition;
 
 attribute vec4 a_position;  // bind this to vertex coordinate array
 attribute vec3 a_normal;    // Array of normals
 attribute vec2 a_texcoord;  // Array of texture coordinates 
 
 varying vec2 v_texcoord;
-varying vec3 View;
-varying vec3 Light;
-varying vec3 Normal;
+varying vec3 N;
+varying vec3 v;
 
+void main(void)  
+{     
+    v = vec3(mv_matrix * a_position);       
+    N = normalize(normalMatrix * a_normal);
 
-void main()
-{
-    vec4 P = mv_matrix * a_position;
-    Light  = lightPosition.xyz - P.xyz;
-    Normal = normalMatrix * a_normal;
-    View  = -P.xyz;
-    v_texcoord = a_texcoord;
-    gl_Position = mvp_matrix * a_position;
+    v_texcoord = a_texcoord;    // texture coordinate pass-through
+    gl_Position = mvp_matrix * a_position;  
 }
+          
